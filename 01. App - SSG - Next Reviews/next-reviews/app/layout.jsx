34ac42f28290d78d0623b1b1555be177507659f2
Daniel Bauer <dan717@gmx.de>
2023-10-01 13:08:58 +0200

    Done learning about usingg the next Link component

--- 01. App - SSG - Next Reviews/next-reviews/app/layout.jsx
+++ 01. App - SSG - Next Reviews/next-reviews/app/layout.jsx
@@ -1,3 +1,6 @@
+// Importing th Link component to use client-side navigation
+import Link from "next/link";
+
 // Next RootLayout Template component
 export default function RootLayout({ children }) {
 	return (
@@ -6,7 +9,27 @@ export default function RootLayout({ children }) {
 				<title>Next.js App</title>
 			</head>
 			<body>
-				<header>[header]</header>
+				<header>
+					<nav>
+						<ul>
+							<li>
+								<Link href="/" prefetch={true}>
+									Home
+								</Link>
+							</li>
+							<li>
+								<Link href="/reviews" prefetch={true}>
+									Reviews
+								</Link>
+							</li>
+							<li>
+								<Link href="/about" prefetch={true}>
+									About
+								</Link>
+							</li>
+						</ul>
+					</nav>
+				</header>
 				<main>{children}</main>
 				<footer>[footer]</footer>
 			</body>
