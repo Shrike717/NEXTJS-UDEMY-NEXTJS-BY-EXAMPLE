7768f0df7d2ed13eee249056e7fbd82ebb9032af
Daniel Bauer <dan717@gmx.de>
2023-10-01 16:32:21 +0200

    Added sub links on rviews page

--- 01. App - SSG - Next Reviews/next-reviews/app/reviews/page.jsx
+++ 01. App - SSG - Next Reviews/next-reviews/app/reviews/page.jsx
@@ -1,8 +1,18 @@
+// Importing th Link component to use client-side navigation
+import Link from "next/link";
+
 export default function ReviewsPage() {
 	return (
 		<>
 			<h1>Reviews</h1>
-			<p>Here we will list all the reviews:</p>
+			<ul>
+				<li>
+					<Link href="/reviews/hollow-knight">Hollow Knight</Link>
+				</li>
+				<li>
+					<Link href="/reviews/stardew-valley">Stardew Valley</Link>
+				</li>
+			</ul>
 		</>
 	);
 }
