b563ff58aa5fc78484970a0865674c59c46f0d3a
Daniel Bauer <dan717@gmx.de>
2023-10-02 09:23:00 +0200

    Installed and configuered Tailwiind

---
+++ 01. App - SSG - Next Reviews/next-reviews/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./app/**/*.{jsx,tsx}", // JSX and TSX files in the src folder
+		"./components/**/*.{jsx,tsx}", // JSX and TSX files in the src folder
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
