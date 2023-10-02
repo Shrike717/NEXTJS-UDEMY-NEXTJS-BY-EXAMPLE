57289a291a020c7b24ef7a0d6c915a9defcec702
Daniel Bauer <dan717@gmx.de>
2023-10-02 10:11:31 +0200

    First positioning of body elements

--- 01. App - SSG - Next Reviews/next-reviews/app/layout.jsx
+++ 01. App - SSG - Next Reviews/next-reviews/app/layout.jsx
@@ -10,10 +10,10 @@ export default function RootLayout({ children }) {
 			<head>
 				<title>Next.js App</title>
 			</head>
-			<body>
+			<body className="flex flex-col px-4 py-2 min-h-screen">
 				<header>
 					<nav>
-						<ul>
+						<ul className="flex gap-2">
 							<li>
 								<Link href="/">Home</Link>
 							</li>
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
 						</ul>
 					</nav>
 				</header>
-				<main>{children}</main>
-				<footer>
+				<main className="py-3 grow">{children}</main>
+				<footer className="border-t py-3 text-center text-xs">
 					Game data and images courtesy of{" "}
 					<a href="https://rawg.io/" target="_blank">
 						RAWG
