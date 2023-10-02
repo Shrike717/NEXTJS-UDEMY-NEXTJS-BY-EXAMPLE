b1047395b7928a711e5f712480d425030ff74127
Daniel Bauer <dan717@gmx.de>
2023-10-02 18:37:52 +0200

    Learned about importing and initializing a  custom ontt from google

---
+++ 01. App - SSG - Next Reviews/next-reviews/app/fonts.js
@@ -0,0 +1,8 @@
+// Imports the Google Fonts Orbitron
+import { Orbitron } from "next/font/google";
+
+// Initialize the Orbitron font. Sets up the font in our app
+// latin is for English
+export const orbitron = Orbitron({
+	subsets: ["latin"],
+});
--- 01. App - SSG - Next Reviews/next-reviews/components/Heading.jsx
+++ 01. App - SSG - Next Reviews/next-reviews/components/Heading.jsx
@@ -1,4 +1,11 @@
+// Importing the font
+import { orbitron } from "@/app/fonts";
+
 // Reusable Heading component
 export default function Heading({ children }) {
-	return <h1 className="font-bold pb-3 text-2xl">{children}</h1>;
+	return (
+		<h1 className={`font-bold pb-3 text-2xl ${orbitron.className}`}>
+			{children}
+		</h1>
+	);
 }
