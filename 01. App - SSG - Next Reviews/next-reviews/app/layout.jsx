df4d90c48b4c37d421c46ec9cb61878e76117054
Daniel Bauer <dan717@gmx.de>
2023-10-02 10:42:17 +0200

    Added bg color and text colors  and  hover style

--- 01. App - SSG - Next Reviews/next-reviews/app/layout.jsx
+++ 01. App - SSG - Next Reviews/next-reviews/app/layout.jsx
@@ -1,5 +1,6 @@
 // Importing th Link component to use client-side navigation
 import Link from "next/link";
+import NavBar from "../components/NavBar";
 // Importing global style css:
 import "./globals.css";
 
@@ -10,28 +11,18 @@ export default function RootLayout({ children }) {
 			<head>
 				<title>Next.js App</title>
 			</head>
-			<body className="flex flex-col px-4 py-2 min-h-screen">
+			<body className="bg-orange-50 flex flex-col px-4 py-2 min-h-screen">
 				<header>
-					<nav>
-						<ul className="flex gap-2">
-							<li>
-								<Link href="/">Home</Link>
-							</li>
-							<li>
-								<Link href="/reviews">Reviews</Link>
-							</li>
-							<li>
-								<Link href="/about" prefetch={false}>
-									About
-								</Link>
-							</li>
-						</ul>
-					</nav>
+					<NavBar />
 				</header>
 				<main className="py-3 grow">{children}</main>
 				<footer className="border-t py-3 text-center text-xs">
 					Game data and images courtesy of{" "}
-					<a href="https://rawg.io/" target="_blank">
+					<a
+						href="https://rawg.io/"
+						target="_blank"
+						className="text-orange-800 hover:underline"
+					>
 						RAWG
 					</a>
 				</footer>
