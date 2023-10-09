80c9d1e0ba2f6d9dd7e61f82d2b28d643f6be772
Daniel Bauer <dan717@gmx.de>
2023-10-09 18:06:39 +0200

    Learned how to make  a static export using the next.config.js file

---
+++ 01. App - SSG - Next Reviews/next-reviews/next.config.js
@@ -0,0 +1,7 @@
+// Exporting an object with common js convention:
+// This tells the editor which properties are available in this object
+/** @type {import(next).nextConfig} */
+
+module.exports = {
+	output: "export",
+};
