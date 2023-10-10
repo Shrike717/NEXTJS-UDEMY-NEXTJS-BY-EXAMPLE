da7041693b5a44a8eb33294393aa04b75feb3185
Daniel Bauer <dan717@gmx.de>
2023-10-10 18:30:13 +0200

    Learned how to filtter the fields we need from the api using the qs library

--- 02. App - Fullstack | CMS - Next Reviews/next-reviews/scripts/strapi-request.mjs
+++ 02. App - Fullstack | CMS - Next Reviews/next-reviews/scripts/strapi-request.mjs
@@ -2,9 +2,25 @@
 
 // Importing fs module to write to file
 import { writeFileSync } from "node:fs";
+// Importing the qs library:
+import qs from "qs";
 
 // Getting all reviews:
-const url = "http://localhost:1337/api/reviews" + "?populate=*";
+const url =
+	"http://localhost:1337/api/reviews" +
+	"?" +
+	qs.stringify(
+		{
+			fields: ["slug", "title", "subtitle", "publishedAt"],
+			populate: { image: { fields: ["url"] } },
+			sort: ["publishedAt:DESC"],
+			pagination: { pageSize: 6 },
+			// populate: "*"
+		},
+		{ encodeValuesOnly: true }
+	); // encodeValuesOnly: Encodes only the values and not the keys
+console.log("url: " + url);
+
 const response = await fetch(url); // fetch is avalable as global function
 const body = await response.json(); // We also need await because it returns a promise
 
