6f3f8a30271ed153c96b0551d09cf389c5ffc6da
Daniel Bauer <dan717@gmx.de>
2023-10-11 12:25:29 +0200

    Done changin the gettReview function step 1

--- 02. App - Fullstack | CMS - Next Reviews/next-reviews/scripts/strapi-request.mjs
+++ 02. App - Fullstack | CMS - Next Reviews/next-reviews/scripts/strapi-request.mjs
@@ -1,25 +1,48 @@
-// In this file we test the endpoints of our Strapi API
+// In this file we test the endpoints of our Strapi API.
+// When the response fits our needs, we can use it in our app.
 
 // Importing fs module to write to file:
 import { writeFileSync } from "node:fs";
 // Importing the qs library:
 import qs from "qs";
 
+// **************
+
 // Getting all reviews:
+// const url =
+// 	"http://localhost:1337/api/reviews" +
+// 	"?" +
+// 	qs.stringify(
+// 		{
+// 			fields: ["slug", "title", "subtitle", "publishedAt"],
+// 			populate: { image: { fields: ["url"] } },
+// 			sort: ["publishedAt:DESC"],
+// 			pagination: { pageSize: 6 },
+// 			// populate: "*"
+// 		},
+// 		{ encodeValuesOnly: true }
+// 	); // encodeValuesOnly: Encodes only the values and not the keys
+// console.log("url: " + url);
+
+// **************
+
+// Getting a single review:
 const url =
 	"http://localhost:1337/api/reviews" +
 	"?" +
 	qs.stringify(
 		{
-			fields: ["slug", "title", "subtitle", "publishedAt"],
+			filters: { slug: { $eq: "hades-2018" } },
+			fields: ["slug", "title", "subtitle", "publishedAt", "body"],
 			populate: { image: { fields: ["url"] } },
-			sort: ["publishedAt:DESC"],
-			pagination: { pageSize: 6 },
+			pagination: { pageSize: 1, withCount: false },
 			// populate: "*"
 		},
 		{ encodeValuesOnly: true }
 	); // encodeValuesOnly: Encodes only the values and not the keys
 console.log("url: " + url);
+
+// **************
 
 const response = await fetch(url); // fetch is avalable as global function
 const body = await response.json(); // We also need await because it returns a promise
