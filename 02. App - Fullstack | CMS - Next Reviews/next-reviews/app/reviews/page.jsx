6b71aac9cf2f3ff7f8a0384ce66743fb23ea7550
Daniel Bauer <dan717@gmx.de>
2023-10-24 09:58:26 +0200

    Started pagination bar on rvvies page and learned aboutt query params

--- 02. App - Fullstack | CMS - Next Reviews/next-reviews/app/reviews/page.jsx
+++ 02. App - Fullstack | CMS - Next Reviews/next-reviews/app/reviews/page.jsx
@@ -9,17 +9,23 @@ export const metadata = {
 	title: "Reviews",
 };
 
-export default async function ReviewsPage() {
+export default async function ReviewsPage({ searchParams }) {
+	// searchParams is the query string from the URL
+	const page = parsePageParam(searchParams.page);
+
 	// Getting all the reviews:
 	const reviews = await getReviews(6);
-	console.log(
-		"[ReviewsPage] rendering:",
-		reviews.map((review) => review.slug).join(", ")
-	);
+	console.log("[ReviewsPage] props:", page);
 
 	return (
 		<>
 			<Heading>Reviews</Heading>
+			{/* Pagination */}
+			<div className="flex gap-2 pb-3">
+				<Link href={`/reviews?page=${page - 1}`}>&lt;</Link>
+				<span>Page {page}</span>
+				<Link href={`/reviews?page=${page + 1}`}>&gt;</Link>
+			</div>
 			<ul className="flex flex-row flex-wrap gap-3 ">
 				{reviews.map((review, index) => (
 					<li
@@ -45,4 +51,15 @@ export default async function ReviewsPage() {
 			</ul>
 		</>
 	);
+}
+
+// Function to parse the query string from the URL:
+function parsePageParam(paramValue) {
+	if (paramValue) {
+		const page = parseInt(paramValue);
+		if (isFinite(page) && page > 0) {
+			return page;
+		}
+	}
+	return 1;
 }
