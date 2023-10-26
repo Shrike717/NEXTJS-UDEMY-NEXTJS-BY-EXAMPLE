b84f78eb0f9d1799555b4cc22ff2340899622af1
Daniel Bauer <dan717@gmx.de>
2023-10-26 14:45:58 +0200

    Done routing to single rviews whn selectingg frrom search box

--- 02. App - Fullstack | CMS - Next Reviews/next-reviews/components/SearchBox.jsx
+++ 02. App - Fullstack | CMS - Next Reviews/next-reviews/components/SearchBox.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Combobox } from "@headlessui/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const reviews = [
 	{ slug: "hades-2018", title: "Hades" },
@@ -12,16 +14,36 @@ const reviews = [
 
 // This component will be used to search for a specific review
 export default function SearchBox() {
+	// Using the Next.js router to get the query string from the URL:
+	const router = useRouter();
+
+	// State to take control of the input value:
+	const [query, setQuery] = useState("");
+	// console.log("Search Query:", query);
+
+	// Filtering the reviews with the query:
+	const filteredReviews = reviews.filter((review) => {
+		return review.title.toLowerCase().includes(query.toLowerCase());
+	});
+
+	// Function to route tto a selected review:
+	function handleChange(review) {
+		// console.log("Selected Review:", review);
+		router.push(`/reviews/${review.slug}`);
+	}
+
 	return (
 		<div className="relative w-48">
-			<Combobox>
+			<Combobox onChange={handleChange}>
 				<Combobox.Input
 					placeholder="Search..."
 					className="w-full rounded border px-2 py-1"
+					value={query}
+					onChange={(event) => setQuery(event.target.value)}
 				/>
 				<Combobox.Options className="absolute w-full bg-white py-1  ">
-					{reviews.map((review) => (
-						<Combobox.Option key={review.slug} value={review.title}>
+					{filteredReviews.map((review) => (
+						<Combobox.Option key={review.slug} value={review}>
 							{({ active }) => (
 								<span
 									className={`block truncate px-2 ${
