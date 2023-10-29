201f4a709eaeef0eca01ee1185db1c73141e39e2
Daniel Bauer <dan717@gmx.de>
2023-10-29 09:32:29 +0100

    Done making the remotePattern confiiguraion configurable

--- 02. App - Fullstack | CMS - Next Reviews/next-reviews/next.config.js
+++ 02. App - Fullstack | CMS - Next Reviews/next-reviews/next.config.js
@@ -3,19 +3,24 @@
 /** @type {import(next).nextConfig} */
 
 module.exports = {
+	// This output: "export", tells Next.js to export the static site into a folder
 	// output: "export",
 	// This allows us to use the Next.js Image component to optimize images
 	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-				port: "1337",
-				pathname: "/uploads/**",
-			},
-		],
+		remotePatterns: [toRemotePattern(process.env.CMS_IMAGE_PATTERN)],
 	},
 	// eslint: {
 	// 	ignoreDuringBuilds: true,
 	// },
 };
+
+// This function makes URLs to lload images c$onfigurable:
+function toRemotePattern(urlString) {
+	const url = new URL(urlString);
+	return {
+		protocol: url.protocol.replace(":", ""),
+		hostname: url.hostname,
+		port: url.port,
+		pathname: url.pathname,
+	};
+}
